c7f148cf980180e4c9f75d7806e5f15dfd08e5fc
philipszdavido <kurtwanger40@gmail.com>
2021-01-10 09:21:27 +0100

    created Header file and code for rendering header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header() {
+    return(
+    <header className="header">
+        <div>
+            <h2>Jur Block-Chain</h2>
+        </div>
+    </header>
+    )
+}
+
+export default Header
