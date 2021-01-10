a6727336fa5191bd896ed03efa09e28fb8cf6714
philipszdavido <kurtwanger40@gmail.com>
2021-01-10 09:19:38 +0100

    added code to render Header and Main components

--- src/App.js
+++ src/App.js
@@ -5,21 +5,9 @@ import Main from "./container/Main"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main {...props} />
     </div>
   );
 }
