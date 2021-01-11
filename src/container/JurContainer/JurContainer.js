1e9461fc668cda1cd2a1971a97d986966e10d647
philipszdavido <kurtwanger40@gmail.com>
2021-01-12 00:47:40 +0100

    made some UI changes

--- src/container/JurContainer/JurContainer.js
+++ src/container/JurContainer/JurContainer.js
@@ -25,17 +25,9 @@ function JurContainer({ loadingStore, drizzle, drizzleState }){
             const _statusTypes = await contract.methods.getStatusTypes().call({ from: drizzleState.accounts[0] });
 
             const jurStatuses = await contract.methods.getStatusList().call({ from: drizzleState.accounts[0] });
-            const _jurStatuses = []
-
-            for (var i = 1; i <= jurStatuses.length; i++) {
-                let _jurStatus = await contract.methods.getJurStatusInfo(jurStatuses[i - 1])
-                .call({from: drizzleState.accounts[0]})
-                _jurStatus.address = jurStatuses[i - 1];
-                _jurStatuses.push(_jurStatus)
-            }
             setJurStatusOpts({
                 statusTypes: _statusTypes,
-                jurStatus: _jurStatuses
+                jurStatus: jurStatuses
             })
         }
     }
