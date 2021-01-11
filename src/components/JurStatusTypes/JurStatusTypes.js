36b6728ea8728f4ff0121ff21dcb9b95e262f750
philipszdavido <kurtwanger40@gmail.com>
2021-01-11 15:42:23 +0100

    added code to help identify a JurStatusType list when selected

--- src/components/JurStatusTypes/JurStatusTypes.js
+++ src/components/JurStatusTypes/JurStatusTypes.js
@@ -1,7 +1,20 @@
-function JurStatusTypes() {
+import "./JurStatusTypes.css"
+import React from "react"
+
+function JurStatusTypes({ jurStatusTypes, onClickHandler = null, highlightIndex=null }) {
+    const highColor = {
+        bgColor:"blueviolet",
+        color: "white",
+    }
+
+    function clHandler() {
+        onClickHandler()
+    }
+
     return (
-        <section className="jurstatuslist">
-        </section>
+        <div className="jurstatustypes-card" style={highlightIndex ? { backgroundColor: highColor.bgColor, color: highColor.color } : null} onClick={clHandler}>
+            {jurStatusTypes}
+        </div>
     )
 }
 
